ce0bb0d95ec00e81d6f927db004789f6d826aefd
Ella Robinson <121602756+ellaroosmarijn@users.noreply.github.com>
2023-04-13 11:49:59 +1200

    Populate seeds.

--- server/db/seeds/1-users.js
+++ server/db/seeds/1-users.js
@@ -1,5 +1,8 @@
 exports.seed = async (knex) => {
   await knex('users').insert([
-   
+    { id: 1, username: 'elisab', auth0id: 'bsd24gyg55w56dd7a'},
+    { id: 2, username: 'ellar', auth0id: '8hskgyg55is830pa'},
+    { id: 3, username: 'biddym', auth0id: '4gyg5bs6dd7ad25w5'},
+    { id: 4, username: 'rhap', auth0id: 'ege666bsd2w5744g20p'}
   ])
 }
--- server/db/seeds/3-collections_artworks.js
+++ server/db/seeds/3-collections_artworks.js
@@ -1,5 +1,12 @@
 exports.seed = async (knex) => {
   await knex('collections_artworks').insert([
-   
+   {collection_id: 1, artwork_id: 2},
+   {collection_id: 1, artwork_id: 4},
+   {collection_id: 2, artwork_id: 1},
+   {collection_id: 2, artwork_id: 2},
+   {collection_id: 2, artwork_id: 3},
+   {collection_id: 2, artwork_id: 4},
+   {collection_id: 3, artwork_id: 2},
+   {collection_id: 4, artwork_id: 1},
   ])
 }
