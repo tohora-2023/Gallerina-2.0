afd0deff3b07a132e90ff5703108f92e77f4e4d5
elisa-bertoni <89889937+elisabertoni@users.noreply.github.com>
2023-04-13 17:07:45 +1200

    display artwork title and image in artwork component

--- client/components/Artworks.tsx
+++ client/components/Artworks.tsx
@@ -1,10 +1,33 @@
+import { useEffect } from 'react'
+
+import { useAppDispatch, useAppSelector } from '../hooks/hooks'
+import { fetchArtworkImage } from '../actions/artworks'
+import { Artwork } from '../../models/artwork'
+
 export default function Home() {
+  const { loading, data, error } = useAppSelector((state) => state.artworkState)
+
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(fetchArtworkImage())
+  }, [dispatch])
 
   return (
-    <>
-      <div>
-        <h1>Home 🖼️</h1>
-      </div>
-    </>
+    <div>
+      {error && <p>{error}</p>}
+      {loading && <img src="/loading-spinner.gif" alt="loading-spinner" />}
+
+      {data?.map((artwork: Artwork) => {
+        return (
+          <div key={artwork.id}>
+            <div>
+              <img src={artwork._links?.thumbnail?.href} alt={artwork.slug} />
+              <div>{artwork.title}</div>
+            </div>
+          </div>
+        )
+      })}
+    </div>
   )
 }
--- models/artwork.ts
+++ models/artwork.ts
@@ -1,12 +1,74 @@
-export interface Artwork {
-    id?: number
-    title?: string
-    artist?: string
-    date?: string
-    category?: string
-    image?: string
-    collecting_institution?: string
+export type Artworks = Artwork[]
 
-  }
-  
-  export type Artworks = Artwork[]
+export interface Artwork {
+  id?:                     string;
+  slug?:                   string;
+  created_at?:             Date;
+  updated_at?:             Date;
+  title?:                  string;
+  category?:               string;
+  medium?:                 string;
+  date?:                   string;
+  dimensions?:             Dimensions;
+  published?:              boolean;
+  website?:                string;
+  signature?:              string;
+  series?:                 null;
+  provenance?:             string;
+  literature?:             string;
+  exhibition_history?:     string;
+  collecting_institution?: string;
+  additional_information?: string;
+  image_rights?:           string;
+  blurb?:                  string;
+  unique?:                 boolean;
+  cultural_maker?:         null;
+  iconicity?:              number;
+  can_inquire?:            boolean;
+  can_acquire?:            boolean;
+  can_share?:              boolean;
+  sale_message?:           string;
+  sold?:                   boolean;
+  image_versions?:         string[];
+  _links?:                 Links;
+//   _embedded?:              Embedded;
+}
+
+// export interface Embedded {
+//   editions: any[];
+// }
+
+export interface Links {
+  thumbnail?:        Artists;
+  image?:            Image;
+  partner?:          Artists;
+  self?:             Artists;
+  permalink?:        Artists;
+  genes?:            Artists;
+  artists?:          Artists;
+  similar_artworks?: Artists;
+  collection_users?: Artists;
+  sale_artworks?:    Artists;
+}
+
+export interface Artists {
+  href: string;
+}
+
+export interface Image {
+  href:      string;
+  templated?: boolean;
+}
+
+export interface Dimensions {
+  in?: CM;
+  cm?: CM;
+}
+
+export interface CM {
+  text?:     string;
+  height?:   number;
+  width?:    number;
+  depth?:    null;
+  diameter?: null;
+}
