763b3331533ebafb4b4f80070ef37d16c3273d0d
Samuel Adams <samueladams@outlook.co.nz>
2023-04-13 14:34:46 +1200

    add auth0 functionality, tailwind styling for navbar

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -7,17 +7,18 @@ import Profile from './Profile'
 import Collection from './Collection'
 
 function App() {
-
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/artwork' element={<Artwork />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/profile/collection' element={<Collection />} />
-        <Route path='/*' element={<></>} />
-      </Routes>
+      <div className="mt-10 mr-10 ml-10 mb-10 flex flex-wrap items-center justify-between bg-gray-500">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/artwork" element={<Artwork />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile/collection" element={<Collection />} />
+          <Route path="/*" element={<></>} />
+        </Routes>
+      </div>
     </>
   )
 }
--- client/components/Profile.tsx
+++ client/components/Profile.tsx
@@ -1,8 +1,30 @@
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useAppDispatch, useAppSelector } from '../hooks'
+// import { fetchResults } from 
+import { useAuth0 } from '@auth0/auth0-react'
+import { KeyboardEvent } from 'react'
+
+
 export default function Profile() {
+  // const { getAccessTokenSilently, isAuthenticated, loginWithRedirect } =
+  // useAuth0()
+
+  // const dispatch = useAppDispatch()
+
+  // useEffect(() => {
+  //   const getAccess = async () => {
+  //     const token = await getAccessTokenSilently()
+  //     dispatch(fetchResults(token))
+  //   }
+  //   getAccess().catch(console.error)
+  // }, [dispatch, getAccessTokenSilently])
+
+
 
   return (
     <>
-  
+  <h1>Test</h1>
     </>
   )
 }
