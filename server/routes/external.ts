d8bc5247cc714c6647746a2bbba6adf429ec2281
Bridget Mahy <biddy.mahy@gmail.com>
2023-04-13 12:33:22 +1200

    gets 500 artworks on external.ts

--- server/routes/external.ts
+++ server/routes/external.ts
@@ -1,27 +1,32 @@
-import res from '../../models/external'
-const request = require('superagent')
+import request from 'superagent'
 import express from 'express'
 const router = express.Router()
 
-const clientID = '0745ecd351bef5ed821f'
-const clientSecret = '2e6e26b06b1046a7d9784540c0c01639'
-const apiUrl = 'https://api.artsy.net/api/tokens/xapp_token'
-
-let xappToken: string
-
-// gets our api token?
-router.get('/api', (req, res) => {
-  request
+// generate xapptoken function
+async function generateXappToken() {
+  const clientID = '0745ecd351bef5ed821f'
+  const clientSecret = '2e6e26b06b1046a7d9784540c0c01639'
+  const apiUrl = 'https://api.artsy.net/api/tokens/xapp_token'
+  return await request
     .post(apiUrl)
     .send({ client_id: clientID, client_secret: clientSecret })
-    .then((res: any) => {
-      xappToken = res.body.token
-      res.json({ token: xappToken })
-    })
-    .catch((err: Error) => {
-      res.sendStatus(500)
-      console.log(err)
-    })
+}
+
+// gets amount of artworks
+router.get('/artworks', async (req, res) => {
+  try {
+    const amount = 500
+    const xapp = await generateXappToken()
+    const response = await request
+      .get(`https://api.artsy.net/api/artworks?size=${amount}`)
+      .set('X-Xapp-Token', xapp.body.token)
+      .set('Accept', 'application/vnd.artsy-v2+json')
+    const artworks = response.body._embedded.artworks
+    res.json(artworks)
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(500)
+  }
 })
 
 export default router
