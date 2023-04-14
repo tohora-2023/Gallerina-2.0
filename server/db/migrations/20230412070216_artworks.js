8c561b6a7ce57c18a8d2585acab16b145f7ab43d
elisa-bertoni <89889937+elisabertoni@users.noreply.github.com>
2023-04-14 12:39:40 +1200

    update migrations"

--- server/db/migrations/20230412070216_artworks.js
+++ server/db/migrations/20230412070216_artworks.js
@@ -4,7 +4,6 @@ exports.up = function (knex) {
     table.string('title')
     table.string('imageLink')
     table.string('medium')
-    table.string('blurb')
     table.string('date')
     table.string('artistLink')
   })
