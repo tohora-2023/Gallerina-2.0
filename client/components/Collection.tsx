530ff6859d1ac28d6f5f9cfdf71219f9af6858e7
Baeley Canning <baeleycanning@hotmail.com>
2023-04-14 16:50:46 +1200

    save

--- client/components/Collection.tsx
+++ client/components/Collection.tsx
@@ -3,24 +3,44 @@ import { useAppDispatch, useAppSelector } from '../hooks/hooks'
 import { fetchCollections } from '../actions/collections'
 
 export default function Collection() {
-  const collections = useAppSelector((state) => state.collections)
+  const {
+    collections: collections,
+    error,
+    isLoading,
+  } = useAppSelector((state) => state.collections)
+
   const dispatch = useAppDispatch()
 
   useEffect(() => {
     dispatch(fetchCollections())
   }, [dispatch])
-  console.log(collections)
+  // console.log(collections)
+
+  //export default interface Collection {
+  //   id?: number
+  //   title: string
+  //   coverImg: string
+  //   userId: number
+  // }
+
+  if (isLoading) return <>Loading...</>
+  if (error) return <>An error occurred</>
+
   return (
     <>
-      <div>
+      {/* <div>
         {collections.loading && (
           <p>Please wait while we load your collections</p>
         )}
         {collections.error && <p>Unfortunately we cannot reach our database</p>}
-      </div>
-      <div>{collections.map((collection) => (
-        <li key={collection.id}>) 
-      }</div>
+      </div> */}
+      <ul>
+        {collections.map((element) => (
+          <li key={element.id}>
+            <span>{element.title}</span>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
