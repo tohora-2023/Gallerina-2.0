7d5744f14b2d1b67f6d781f3a448630e23a65fbf
Baeley Canning <baeleycanning@hotmail.com>
2023-04-19 12:23:24 +1200

    make adjustments to collection title

--- client/components/Collection.tsx
+++ client/components/Collection.tsx
@@ -22,15 +22,16 @@ export default function Collection(profile: Props) {
   const [showForm, setShowForm] = useState(false) // Add state variable
 
   const handleDeleteClick = async () => {
-    if(confirm("Are you sure you want to delete?")) {
-    const token = await getAccessTokenSilently()
-    dispatch(deleteCollection(profile.collectionId, token))
-  } else {
-    return
-  }
+    if (confirm('Are you sure you want to delete?')) {
+      const token = await getAccessTokenSilently()
+      dispatch(deleteCollection(profile.collectionId, token))
+    } else {
+      return
+    }
   }
 
   const handleUpdateClick = async () => {
+    console.log('clicked')
     setShowForm(true) // Display the form when the button is clicked
   }
 
@@ -43,71 +44,73 @@ export default function Collection(profile: Props) {
 
   return (
     <>
-    <Link to={`/collections/${profile.collectionId}`}>
-      <div className="flex justify-center mt-10 mb-10">
-        <div className="shadow-xs border-grey w-full transform cursor-pointer rounded-lg border bg-white text-center font-bold tracking-wide text-black transition duration-200 hover:-translate-y-1 hover:bg-my-gold hover:shadow-2xl active:translate-y-0 active:shadow-xl">
-          <div className="grid grid-cols-2 gap-4">
-          <img
-            className="mt-10 mb-10 self-align-left mx-auto"
-            src={profile.collectionCoverImg}
-            alt={`cover for ${profile.title}`}
-          />
-            <div className='mt-10 text-3xl'>
-          
-            {profile.title}
-          
-          <div className="mt-10 flex justify-center text-lg">
-            <button className="shadow-xs mx-2 mb-2 transform cursor-pointer rounded-full border border-black bg-white px-2 py-0.5 font-bold tracking-wide text-black transition duration-200 hover:-translate-y-1 hover:bg-my-gold hover:shadow-2xl active:translate-y-0 active:shadow-xl">
-              <FontAwesomeIcon icon={faTrash} onClick={handleDeleteClick} />
-            </button>
-            <button className="shadow-xs mx-2 mb-2 transform cursor-pointer rounded-full border border-black bg-white px-2 py-0.5 font-bold tracking-wide text-black transition duration-200 hover:-translate-y-1 hover:bg-my-gold hover:shadow-2xl active:translate-y-0 active:shadow-xl">
-              <FontAwesomeIcon icon={faPen} onClick={handleUpdateClick} />
-            </button>
-          </div>
-          
-          </div>
-          
-          </div>
-          
-          
+      
+        <div className="mt-10 mb-10 flex justify-center">
+          <div className="shadow-xs border-grey w-full transform cursor-pointer rounded-lg border bg-white text-center font-bold tracking-wide text-black transition duration-200 hover:-translate-y-1 hover:bg-my-gold hover:shadow-2xl active:translate-y-0 active:shadow-xl">
+            <div className="grid grid-cols-2 gap-4 items-center pt-10 pb-10">
+              <img
+                className="self-align-left mx-auto rounded-md h-full"
+                src={profile.collectionCoverImg}
+                alt={`cover for ${profile.title}`}
+              />
+              <div className="text-3xl self-center">
+              <Link to={`/collections/${profile.collectionId}`}>
+                {profile.title}
+                </Link>
+                <div className="flex justify-center text-base">
+                  <button
+                    onClick={handleDeleteClick}
+                    className="shadow-xs mx-2 mb-2 transform cursor-pointer rounded-full border border-black bg-white px-2 py-0.5 font-bold tracking-wide text-black transition duration-200  hover:bg-my-gold hover:shadow-2xl active:translate-y-0 active:shadow-xl"
+                  >
+                    <FontAwesomeIcon icon={faTrash} /> Delete
+                  </button>
+                  <button
+                    onClick={handleUpdateClick}
+                    className="z-10 shadow-xs mx-2 mb-2 transform cursor-pointer rounded-full border border-black bg-white px-2 py-0.5 font-bold tracking-wide text-black transition duration-200  hover:bg-my-gold hover:shadow-2xl active:translate-y-0 active:shadow-xl"
+                  >
+                    <FontAwesomeIcon icon={faPen} /> Rename
+                  </button>
+                </div>
+              </div>
+            </div>
 
-          {showForm && (
-            <form onSubmit={handleUpdateSubmit} className="mt-4">
-              <div className="mb-4 flex flex-col">
-                <label htmlFor="name" className="mb-2 text-center font-bold">
-                  New Collection Name:
-                </label>
-                <input maxlength="40"
-                  type="text"
-                  name="name"
-                  id="name"
-                  className="text-grey-darkest border py-2 px-3"
-                  value={amendTitle}
-                  required
-                  onChange={(e) => setAmendTitle(e.target.value)}
-                />
-              </div>
-              <div className="flex justify-between">
-                <button
-                  type="submit"
-                  className="shadow-xs mx-2 transform cursor-pointer rounded-full border border-black bg-white px-2 py-0.5 font-bold tracking-wide text-black transition duration-200 hover:-translate-y-1 hover:bg-my-gold hover:shadow-2xl active:translate-y-0 active:shadow-xl"
-                >
-                  Update
-                </button>
-                <button
-                  type="button"
-                  onClick={() => setShowForm(false)}
-                  className="shadow-xs mx-2 transform cursor-pointer rounded-full border border-black bg-white px-2 py-0.5 font-bold tracking-wide text-black transition duration-200 hover:-translate-y-1 hover:bg-my-gold hover:shadow-2xl active:translate-y-0 active:shadow-xl"
-                >
-                  Cancel
-                </button>
-              </div>
-            </form>
-          )}
+            {showForm && (
+              <form onSubmit={handleUpdateSubmit} className="mt-4">
+                <div className="mb-4 flex flex-col">
+                  <label htmlFor="name" className="mb-2 text-center font-bold">
+                    New Collection Name:
+                  </label>
+                  <input
+                    maxLength="40"
+                    type="text"
+                    name="name"
+                    id="name"
+                    className="text-grey-darkest border py-2 px-3"
+                    value={amendTitle}
+                    required
+                    onChange={(e) => setAmendTitle(e.target.value)}
+                  />
+                </div>
+                <div className="flex justify-between">
+                  <button
+                    type="submit"
+                    className="shadow-xs mx-2 transform cursor-pointer rounded-full border border-black bg-white px-2 py-0.5 font-bold tracking-wide text-black transition duration-200 hover:-translate-y-1 hover:bg-my-gold hover:shadow-2xl active:translate-y-0 active:shadow-xl"
+                  >
+                    Update
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => setShowForm(false)}
+                    className="shadow-xs mx-2 transform cursor-pointer rounded-full border border-black bg-white px-2 py-0.5 font-bold tracking-wide text-black transition duration-200 hover:-translate-y-1 hover:bg-my-gold hover:shadow-2xl active:translate-y-0 active:shadow-xl"
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </form>
+            )}
+          </div>
         </div>
-        
-      </div>
-      </Link>
+      
     </>
   )
 }
